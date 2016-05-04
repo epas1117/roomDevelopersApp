f6b6532e8fe5437fe5e1dd343d415ac597ae81ad
ESTEBAN <earistizabal540@hotmail.com>
2016-05-03 22:10:17 -0500

    ELiminado archivos innecesarios del tutorial de laravel hasta el video 18, se quedan el modelo user, controlador user y vistas de user para basarnos en nuestras tareas.

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -15,86 +15,12 @@ class FrontController extends Controller
      */
     public function index()
     {
-        return view('index');
+        return view('intro');
     }
-    
-    public function contacto()
-    {
-        return view('contacto');
-    }
-    
-    public function reviews()
-    {
-        return view('reviews');
-    }
-    
+
     public function admin()
     {
         return view('admin.index');
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- resources/views/usuario/edit.blade.php
+++ resources/views/usuario/edit.blade.php
@@ -9,7 +9,7 @@
     {!!Form::submit('Actualizar',['class'=>'btn btn-primary'])!!}
 
     {!!Form::close()!!}
-
+    <br>
     {!!Form::open(['route'=>['usuario.destroy',$user->id],'method'=>'DELETE'])!!}
 
     {!!Form::submit('Eliminar',['class'=>'btn btn-danger'])!!}
