211e6dd886291578de1647b1851edce2483f2b0a
robcga1 <robcga1@gmail.com>
2016-05-07 16:45:11 -0500

    Se establecio las interfaces de seccion, añadi un registro para pruebas, se lo puede eliminar cuando deseen2

--- resources/views/seccion/index.blade.php
+++ resources/views/seccion/index.blade.php
@@ -10,8 +10,8 @@
     </thread>
     @foreach($secciones as $seccion)
         <tbody >
-        <td >{{$seccion->id}} </td>
-        <div id="{{$seccion->id}}" class="collapse">AQUI SE MOSTRARAN DETALLES</div>
+        <td >{{$seccion->id}} <div id="{{$seccion->id}}" class="collapse">AQUI SE MOSTRARAN DETALLES</div></td>
+
         <td>{{$seccion->titulo}}</td>
         <td>{{$seccion->tutorial_id}}</td>
 
