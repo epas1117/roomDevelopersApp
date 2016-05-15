acb8f5ce3033f9fb722c87131e16206a7a7ab771
robcga1 <robcga1@gmail.com>
2016-05-15 10:10:25 -0500

    9:00 am casi terminado

---
+++ resources/views/seccion/respaldovideoSeccion.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+@section('content')
+
+
+
+<!------------------>
+<div id="accordion" role="tablist" aria-multiselectable="true">
+
+    <div class="panel panel-default">
+        @foreach($secciones as $seccion)
+        <div class="panel-heading" role="tab" id="headingOne">
+            <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    {{$seccion->titulo}}
+                </a>
+            </h4>
+
+        </div>
+
+        @foreach($videos as $video)
+        @if($seccion->id===$video->seccion_id)
+        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+            <!---------------->
+            <p>  {{$video->id}} </p>
+
+            <p> {{$video->titulo}}</p>
+            <p>{{$video->duracion}}</p>
+
+            <!---------------->
+        </div>
+        @endif
+        @endforeach
+    </div>
+
+    @endforeach
+</div>
+<!------------------>
+
+
+@endsection
