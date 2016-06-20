b078853954ee10b6b44a0b5f9b432dffe2cc8549
Cristal <anafriends19@hotmail.com>
2016-06-19 23:39:40 -0500

    PUSHH FINAL cris

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,8 +30,8 @@
                 <ul class="nav navbar-nav pull-right">
                     @if(!Auth::check())
                         <li><a class="text-center" href="#" data-toggle="modal"
-                               data-target="#signUpModal">Regístrate</a></li>
-                        <li><a class="text-center" href="#" data-toggle="modal" data-target="#logInModal">Inicia
+                               data-target="#signUpModal"style="color: #ffffff;">Regístrate</a></li>
+                        <li><a class="text-center" href="#" data-toggle="modal" data-target="#logInModal"style="color: #FFFFFF" >Inicia
                                 Sesión</a></li>
                     @else
                         <li><a class="text-center" href="{{URL::to('usuario/perfil')}}">{{Auth::user()->name}}</a></li>
@@ -46,27 +46,28 @@
     @yield('content')
 </section>
 
-<nav class="navbar navbar-inverse navbar-bottom">
+<footer>
+<nav class="navbar navbar-static-top navbar-bottom">
     <div class="container">
         <div class="row">
-            <div id="navbar" class="collapse navbar-collapse">
+            <div id="navbarc" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Principal</a></li>
+                    <li class="active"><a href="#" style="color: #ffffff">Principal</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                           aria-expanded="false">Acerca de roomDevelopers<span class="caret"></span></a>
+                           aria-expanded="false" style="color: #ffffff">Acerca de roomDevelopers<span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Pagina Web</a></li>
-                            <li><a href="#">Equipo</a></li>
+                            <li><a href="#" style="color: #ffffff" >Pagina Web</a></li>
+                            <li><a href="#" style="color: #ffffff">Equipo</a></li>
                         </ul>
                     </li>
-                    <li><a href="#contact">Contáctenos</a></li>
+                    <li><a href="#contact" style="color: #ffffff">Contáctenos</a></li>
                 </ul>
             </div>
         </div>
     </div>
 </nav>
-
+</footer>
 {!!Html::script('js/jquery.min.js')!!}
 {!!Html::script('js/bootstrap.min.js')!!}
 {!!Html::script('js/scriptsrd.js')!!}
