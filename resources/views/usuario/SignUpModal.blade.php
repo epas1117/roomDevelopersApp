d3cb0ef61aa133d92d85e3f0ad1b915ef034d0ed
Cristal <anafriends19@hotmail.com>
2016-06-20 00:33:22 -0500

    PUSHH FINAL crisita

--- resources/views/usuario/SignUpModal.blade.php
+++ resources/views/usuario/SignUpModal.blade.php
@@ -4,21 +4,21 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                             aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Sign Up</h4>
+                <h4 class="modal-title" id="myModalLabel">Registrarse</h4>
             </div>
             <div class="modal-body">
                 {!!Form::open(['route'=> ['usuario.store'],'method'=>'POST'])!!}
                 <div >
-                    <input name="name" type="text" class="form-control" placeholder="Your name...">
-                    <input name="email" type="text" class="form-control" placeholder="Email...">
-                    <input name="password" type="password" class="form-control" placeholder="Password...">
+                    <input name="name" type="text" class="form-control" placeholder="Nombre...">
+                    <input name="email" type="text" class="form-control" placeholder="Correo...">
+                    <input name="password" type="password" class="form-control" placeholder="Contraseña...">
                 </div>
                 <div class="modal-footer">
                     <button class="btn btn-block" type="submit">
-                        Sign Up
+                        Registrarse
                     </button>
                     <a class="btn btn-block btn-social btn-facebook" href="{{URL::to('auth/facebook')}}">
-                        <i class="fa fa-facebook"></i> Sign up with Facebook
+                        <i class="fa fa-facebook"></i> Registrarse con Facebook
                     </a>
                 </div>
                 {!!Form::close()!!}
